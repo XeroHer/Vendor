b1adf8c317cccce94760358d9a31b216539f68f2
XeroHer <bksraut27@gmail.com>
2026-04-30 22:51:30 +0100

    changed jsx to tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import React from 'react'
 import { ProductProvider } from './Home/ProductContext.tsx'
-import {ToggleProvider} from './Vendor/ThemeProvider'
+import {ToggleProvider} from './Vendor/ThemeProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
    <React.StrictMode>
